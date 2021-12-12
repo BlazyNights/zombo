dc0549a107d6d0a9b2b9e6671f3c7a2a69fb92e3
BlazyNights <BlazyNights@gmail.com>
2021-12-12 01:48:57 -0800

    Wrapped the code into main() and added try/except to stop for ctrl-c presses

--- zombo.py
+++ zombo.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import shlex
 import configparser
+import sys
 
 
 def run_command(__command: str, print_only=False):
@@ -20,43 +21,51 @@ def run_command(__command: str, print_only=False):
         subprocess.run(__command, check=True)
 
 
-# Read config file
-parser = configparser.ConfigParser()
-parser.read('zombo.ini')
+def main():
+    # Read config file
+    parser = configparser.ConfigParser()
+    parser.read('zombo.ini')
 
-preview_mode = bool(int(parser.get('preview', 'print_only')))
+    preview_mode = bool(int(parser.get('preview', 'print_only')))
 
-ffmpeg_path = parser.get('ffmpeg', 'path')
-ffmpeg_args = parser.get('ffmpeg', 'args')
+    ffmpeg_path = parser.get('ffmpeg', 'path')
+    ffmpeg_args = parser.get('ffmpeg', 'args')
 
-input_path = parser.get('input', 'path')
-# Filter empty entries, turn it into a list
-input_list = list(filter(None, parser.get('input', 'files').splitlines()))
+    input_path = parser.get('input', 'path')
+    # Filter empty entries, turn it into a list
+    input_list = list(filter(None, parser.get('input', 'files').splitlines()))
 
-use_additional_files = bool(int(parser.get('additional_files', 'use_additional_files')))
-additional_files_path = parser.get('additional_files', 'path')
-additional_files_list = list(filter(None, parser.get('additional_files', 'files').splitlines()))
+    use_additional_files = bool(int(parser.get('additional_files', 'use_additional_files')))
+    additional_files_path = parser.get('additional_files', 'path')
+    additional_files_list = list(filter(None, parser.get('additional_files', 'files').splitlines()))
 
-output_path = parser.get('output', 'path')
-output_list = list(filter(None, parser.get('output', 'files').splitlines()))
-if not os.path.isdir(output_path):
-    os.mkdir(output_path)
+    output_path = parser.get('output', 'path')
+    output_list = list(filter(None, parser.get('output', 'files').splitlines()))
+    if not os.path.isdir(output_path):
+        os.mkdir(output_path)
+
+    # Run the commands, mapping the input and output lists 1:1 (or 1:1:1 if using additional files)
+    if use_additional_files:
+        for in_item, additional_file, out_item in zip(input_list, additional_files_list, output_list):
+            command = f'"{ffmpeg_path}"' \
+                      f' -i "{os.path.join(input_path, in_item)}"' \
+                      f' -i "{os.path.join(additional_files_path, additional_file)}"' \
+                      f' {ffmpeg_args}' \
+                      f' "{os.path.join(output_path, out_item)}"'
+            run_command(command, print_only=preview_mode)
+
+    else:
+        for in_item, out_item in zip(input_list, output_list):
+            command = f'"{ffmpeg_path}"'\
+                      f' -i "{os.path.join(input_path, in_item)}"'\
+                      f' {ffmpeg_args}'\
+                      f' "{os.path.join(output_path, out_item)}"'
+            run_command(command, print_only=preview_mode)
 
 
-# Run the commands, mapping the input and output lists 1:1 (or 1:1:1 if using additional files)
-if use_additional_files:
-    for in_item, additional_file, out_item in zip(input_list, additional_files_list, output_list):
-        command = f'"{ffmpeg_path}"' \
-                  f' -i "{os.path.join(input_path, in_item)}"' \
-                  f' -i "{os.path.join(additional_files_path, additional_file)}"' \
-                  f' {ffmpeg_args}' \
-                  f' "{os.path.join(output_path, out_item)}"'
-        run_command(command, print_only=preview_mode)
-
-else:
-    for in_item, out_item in zip(input_list, output_list):
-        command = f'"{ffmpeg_path}"'\
-                  f' -i "{os.path.join(input_path, in_item)}"'\
-                  f' {ffmpeg_args}'\
-                  f' "{os.path.join(output_path, out_item)}"'
-        run_command(command, print_only=preview_mode)
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print('Stopping...')
+        sys.exit(-1)
